de7eec487f66cad6b154476e15848c6067f7a623
ParnThanatibordee <parn8946@gmail.com>
2022-10-25 01:35:31 +0700

    connectRedis

---
+++ src/utils/connectRedis.ts
@@ -0,0 +1,24 @@
+import { createClient } from 'redis';
+import logger from "@src/utils/logger";
+
+const redisUrl = `redis://localhost:6379`;
+const redisClient = createClient({
+    url: redisUrl,
+});
+
+const connectRedis = async () => {
+    try {
+        await redisClient.connect();
+        logger.info('Redis client connected...');
+    } catch (err: any) {
+        logger.error(err.message);
+        setTimeout(connectRedis, 5000);
+    }
+};
+
+connectRedis();
+
+redisClient.on('error', (err) => logger.error(err));
+
+export default redisClient;
+
