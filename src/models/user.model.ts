b4883763aec0e54460a28e5f6d7fa3db9ea37c6d
ParnThanatibordee <parn8946@gmail.com>
2022-10-25 02:01:12 +0700

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import {
+    DocumentType,
+    getModelForClass,
+    index,
+    modelOptions,
+    pre,
+    prop,
+} from '@typegoose/typegoose';
+import bcrypt from 'bcryptjs';
+
+@pre<User>('save', async function () {
+    if (!this.isModified('laserCode')) return;
+
+    this.laserCode = await bcrypt.hash(this.laserCode, 12);
+})
+@modelOptions({
+    schemaOptions: {
+    timestamps: true,
+    },
+})
+  
+
+export class User {
+    @prop({ required: true, unique: true, minlength: 13, maxLength: 13 })
+    citizenID!: string;
+
+    @prop({ required: true, minlength: 1, select: false })
+    laserCode!: string;
+
+    async comparePasswords(hashedPassword: string, candidatePassword: string) {
+      return await bcrypt.compare(candidatePassword, hashedPassword);
+    }
+}
+
+const userModel = getModelForClass(User);
+export default userModel;
+  
+  
