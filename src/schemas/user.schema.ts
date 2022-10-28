2034c1d445d17982920c37a9b9d20da3cb60edd5
ParnThanatibordee <parn8946@gmail.com>
2022-10-29 00:07:20 +0700

    edit user.schema (add email & password in create and login Schema)

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -7,16 +7,28 @@ export const createUserSchema = object({
       ).min(13, 'citizenID must be exactly 13 characters')
        .max(13, 'citizenID must be exactly 13 characters'),
     laserCode: string({required_error: 'LaserCode is required'}),
-  })
+    email: string({ required_error: 'Email is required' }).email(
+      'Invalid email'
+    ),
+    password: string({ required_error: 'Password is required' })
+      .min(8, 'Password must be more than 8 characters')
+      .max(32, 'Password must be less than 32 characters'),
+    passwordConfirm: string({ required_error: 'Please confirm your password' }),
+  }).refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: 'Passwords do not match',
+  }),
 });
 
 export const loginUserSchema = object({
   body: object({
-    citizenID: string({ required_error: 'Email is required' }).email(
-      'Invalid citizenID or laserCode'
-    ).min(13, 'citizenID must be exactly 13 characters')
-     .max(13, 'citizenID must be exactly 13 characters'),
-    laserCode: string({ required_error: 'LaserCode is required' }),
+    email: string({ required_error: 'Email is required' }).email(
+      'Invalid email or password'
+    ),
+    password: string({ required_error: 'Password is required' }).min(
+      8,
+      'Invalid email or password'
+    ),
   }),
 });
 
