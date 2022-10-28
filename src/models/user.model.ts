e1388a2f9acee6479e177206db5a4f9b67977377
ParnThanatibordee <parn8946@gmail.com>
2022-10-29 00:01:36 +0700

    edit user.model (add email & password)

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -10,9 +10,9 @@ import bcrypt from 'bcryptjs';
 
 @index({ citizenID: 1 })
 @pre<User>('save', async function () {
-    if (!this.isModified('laserCode')) return;
+    if (!this.isModified('password')) return;
 
-    this.laserCode = await bcrypt.hash(this.laserCode, 12);
+    this.password = await bcrypt.hash(this.password, 12);
 })
 @modelOptions({
     schemaOptions: {
@@ -25,8 +25,14 @@ export class User {
     @prop({ required: true, unique: true, minlength: 13, maxLength: 13 })
     citizenID!: string;
 
-    @prop({ required: true, minlength: 1, select: false })
+    @prop({ required: true})
     laserCode!: string;
+
+    @prop({ required: true, unique: true })
+    email!: string;
+
+    @prop({ required: true, minlength: 8, maxLength: 32, select: false })
+    password!: string;
 
     async comparePasswords(hashedPassword: string, candidatePassword: string) {
       return await bcrypt.compare(candidatePassword, hashedPassword);
