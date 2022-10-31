06f2bc36dfa94e098c46b26ea5d61307e10a3d4b
Thanathip <dannajaeiei@hotmail.com>
2022-10-31 23:41:06 +0700

    fix: health check path and test

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use(
   }),
 )
 
-app.use('/health', healthRouter)
+app.use('/api/health', healthRouter)
 app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter)
 
--- tests/integration/health.test.ts
+++ tests/integration/health.test.ts
@@ -3,9 +3,10 @@ import app from '../../src/app'
 
 describe('Test /health', () => {
   it('health should be okay', async () => {
-    const response = await request(app).get('/health')
+    const response = await request(app).get('/api/health')
     expect(response.statusCode).toBe(200)
     expect(response.error).toBe(false)
-    expect(response.text).toBe('Voter Backend')
+    expect(response.body.status).toBe('success')
+    expect(response.body.message).toBe('Voter Backend')
   })
 })
