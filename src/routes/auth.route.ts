0c33dbbc5e5f5aac52b887fc7fec6e9b20dea434
Thanathip <dannajaeiei@hotmail.com>
2022-10-31 23:27:35 +0700

    feat: added docs of authentication and user api

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,7 +5,71 @@ import { createUserSchema, loginUserSchema } from '../schemas/user.schema'
 
 const router = Router()
 
-// Register user route
+/**
+ * @openapi
+ * '/auth/register':
+ *  post:
+ *     tags:
+ *     - Auth
+ *     summary: Register
+ *     description: Register a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/CreateUserInput'
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CreateUserResponse'
+ *      400:
+ *        description: Bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ErrorResponse'
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ErrorResponse'
+ * '/auth/login':
+ *  post:
+ *     tags:
+ *     - Auth
+ *     summary: Login
+ *     description: Login a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/LoginUserInput'
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LoginUserResponse'
+ *      400:
+ *        description: Bad request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ErrorResponse'
+ *      500:
+ *        description: Internal Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/ErrorResponse'
+ */
 router.post('/register', validate(createUserSchema), registerHandler)
 
 // Login user route
