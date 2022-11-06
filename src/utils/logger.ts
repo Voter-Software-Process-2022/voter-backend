62e0c927017397151a28c04c806eac761d491793
tboonma <tawan.boonma@agoda.com>
2022-11-06 16:54:52 +0700

    Try use pino as stream

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,20 @@
 import pino from 'pino'
+import pretty, { PrettyOptions } from 'pino-pretty'
 import dayjs from 'dayjs'
 
-const log = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-  base: {
-    pid: false,
-  },
-  timestamp: () => `,"time":"${dayjs().format()}"`,
-})
+// const log = pino({
+//   transport: {
+//     target: 'pino-pretty',
+//     options: {
+//       colorize: true,
+//     },
+//   },
+//   base: {
+//     pid: false,
+//   },
+//   timestamp: () => `,"time":"${dayjs().format()}"`,
+// })
+
+const log = pino(pretty())
 
 export default log
