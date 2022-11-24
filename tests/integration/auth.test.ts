e1b9fb49685017be85ffd881b66ca1f87f616d9c
ParnThanatibordee <parn8946@gmail.com>
2022-11-25 01:34:45 +0700

    add test gov module api

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -4,100 +4,127 @@ import app from '../../src/app'
 
 const GOVERNMENT_HOST = moduleHosts.government
 
-describe('Test /auth/register', () => {
-  it('POST / => create NEW user', async () => {
-    // const response = await request(app).post('/api/auth/register')
-    // .send({
-    //     citizenID: '1111111111111',
-    //     laserCode: 'JC0000000000',
-    //     email: 'johndoe@gmail.com',
-    //     password: 'password123',
-    //     passwordConfirm: 'password123',
-    // })
-    // expect(response.statusCode).toBe(201)
-    // expect(response.error).toBe(false)
-    // expect(response.body.status).toBe('Created')
+// Test /auth/register
+// Test /auth/login
 
-    // expect(response.body).toEqual(
-    //     expect.objectContaining({
-    //         status: 'success',
-    //         data: {
-    //           user: {
-    //             citizenID: '1111111111111',
-    //             laserCode: 'JC0000000000',
-    //             email: 'johndoe@gmail.com',
-    //           } 
-    //         }
-    //     })
-    // )
+describe('Test gov /auth/login', () => {
+  it('POST / => login with valid user', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "1234567891234",
+      lazerID: "1234AB"
+    })
+    expect(res.status).toBe(200)
+    expect(res.error).toBe(false)
+    expect(res.body.token).not.toBeNull()
+  });
+
+  it('POST / => login with invalid user CitizenID', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "A234567891234",
+      lazerID: "1234AB"
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
+  });
+
+  it('POST / => login with exceed CitizenID digits', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "12345678912345",
+      lazerID: "1234AB"
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
+  });
+
+  it('POST / => login with invalid user lazerID', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "1234567891234",
+      lazerID: "1234ABC"
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
+  });
+
+  it('POST / => login with empty CitizenID', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "",
+      lazerID: "1234AB"
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
+  });
+
+  it('POST / => login with empty lazerID', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "1234567891234",
+      lazerID: ""
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
+  });
+
+  it('POST / => login with both field empty', async () => {
+    const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
+      CitizenID: "",
+      lazerID: ""
+    })
+    expect(res.status).toBe(401)
+    expect(res.error).not.toBe(false)
+    expect(res.body).toEqual(
+      expect.objectContaining({
+          message: "Can't authenticate with this citizenID and lazerID"
+      })
+    )
   });
 })
 
-
-// use mock
 describe('Test /auth/login/v2', () => {
   it('POST / => login with valid user', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '',
-      LaserID: '',
-    })
-    expect(response.statusCode).toBe(201)
-    expect(response.error).toBe(false)
-    expect(response.body.status).toBe('Created')
-
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        status: 'success',
-        data: {
-          user: {
-            citizenID: '1111111111111',
-            laserCode: 'JC0000000000',
-            email: 'johndoe@gmail.com',
-          } 
-        }
-      })
-    )
-    // token not empty
+    
   }) 
   
   it('POST / => login with invalid user, CitizenID exceed 13 digits', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '12345678910111213',
-      LaserID: '',
-    })
+
   })
   
   it('POST / => login with invalid user, CitizenID contain string', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '1A23456789101',
-      LaserID: '',
-    })
+
   }) 
 
   it('POST / => login with invalid user, CitizenID field is empty', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '',
-      LaserID: '#',
-    })
+
   }) 
 
   it('POST / => login with invalid user, LaserID', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '1111111111111',
-      LaserID: 'invalid123',
-    })
+
   }) 
 
   it('POST / => login with invalid user, LaserID field is empty', async () => {
-    const response = await request(app).post(`${GOVERNMENT_HOST}/user/info`)
-    .send({
-      CitizenID: '1111111111111',
-      LaserID: '',
-    })
+
   }) 
 })
