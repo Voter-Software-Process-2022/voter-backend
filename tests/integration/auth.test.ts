5b4ea00ff5778067f5002743948fb435de7eca4d
ParnThanatibordee <parn8946@gmail.com>
2022-12-01 21:03:26 +0700

    edit auth.test.ts

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -25,7 +25,7 @@ describe('Test gov /auth/login', () => {
   it('POST / => login with invalid user CitizenID', async () => {
     const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
       CitizenID: "A234567891234",
-      lazerID: "1234AB"
+      lazerID: "JT9999999998"
     })
     expect(res.status).toBe(401)
     expect(res.error).not.toBe(false)
@@ -39,7 +39,7 @@ describe('Test gov /auth/login', () => {
   it('POST / => login with exceed CitizenID digits', async () => {
     const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
       CitizenID: "12345678912345",
-      lazerID: "1234AB"
+      lazerID: "JT9999999998"
     })
     expect(res.status).toBe(401)
     expect(res.error).not.toBe(false)
@@ -53,7 +53,7 @@ describe('Test gov /auth/login', () => {
   it('POST / => login with invalid user lazerID', async () => {
     const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
       CitizenID: "2222222222222",
-      lazerID: "1234ABC"
+      lazerID: "JT9999999999"
     })
     expect(res.status).toBe(401)
     expect(res.error).not.toBe(false)
@@ -67,7 +67,7 @@ describe('Test gov /auth/login', () => {
   it('POST / => login with empty CitizenID', async () => {
     const res = await request(`${GOVERNMENT_HOST}`).post('/auth/login/').send({
       CitizenID: "",
-      lazerID: "1234AB"
+      lazerID: "JT9999999998"
     })
     expect(res.status).toBe(401)
     expect(res.error).not.toBe(false)
@@ -116,7 +116,7 @@ describe('Test gov /user/info', () => {
     expect(res.error).toBe(false)
     expect(res.body.token).not.toBeNull()
 
-    const res1 = await request(`${GOVERNMENT_HOST}`).get('/user/info/').set('Authorization', `Bearer ${res.body.token}`)
+    const res1 = await request(`${GOVERNMENT_HOST}`).get('/user/info').set('Authorization', `Bearer ${res.body.token}`)
     expect(res1.status).toBe(200)
     expect(res1.error).toBe(false)
     expect(res.body.CitizenID).not.toBeNull()
@@ -129,12 +129,12 @@ describe('Test gov /user/info', () => {
   });
 
   it('GET / => request with invalid TOKEN', async () => {
-    const res = await request(`${GOVERNMENT_HOST}`).get('/user/info/').set('Authorization', `Bearer abc`)
+    const res = await request(`${GOVERNMENT_HOST}`).get('/user/info').set('Authorization', `Bearer abc`)
     expect(res.status).toBe(401)
     expect(res.error).not.toBe(false)
     expect(res.body).toEqual(
       expect.objectContaining({
-          message: "Can't authenticate with this citizenID and lazerID"
+          message: "token contains an invalid number of segments"
       })
     )
   });
