af107e59572b3fa2afd800c48356cd985c0a1ab9
ParnThanatibordee <parn8946@gmail.com>
2022-12-01 13:51:50 +0700

    implement /party test

--- tests/integration/candidate.test.tsx
+++ tests/integration/candidate.test.tsx
@@ -9,8 +9,9 @@ const validAreaId = 1
 
 const validCandidatesId = 44
 
-describe('Test ec /candidates', () => {
+const validPartyId = 1
 
+describe('Test ec /candidates', () => {
     it('GET / => get all candidates info', async () => {
         const res = await request(`${ELECTION_COMMITTEE_HOST}`).get('/candidates')
         expect(res.status).toBe(200)
@@ -45,7 +46,7 @@ describe('Test ec /candidates/area/{areaId}', () => {
         }
       });
 
-      it('GET / => get candidates info in invalid area', async () => {
+    it('GET / => get candidates info in invalid area', async () => {
         const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/candidates/area/9`)
         expect(res.status).toBe(200)
         expect(res.error).toBe(false)
@@ -53,7 +54,7 @@ describe('Test ec /candidates/area/{areaId}', () => {
         expect(res.body.length).toBe(0)
       });
 
-      it('GET / => get candidates info in invalid area (char)', async () => {
+    it('GET / => get candidates info in invalid area (char)', async () => {
         const invalidAreaIdChar = 'a'
         const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/candidates/area/${invalidAreaIdChar}`)
         expect(res.status).toBe(422)
@@ -75,7 +76,7 @@ describe('Test ec /candidates/{id}', () => {
         expect(res.body.area_id).toBe(validAreaId)    
       });
 
-      it('GET / => get candidates info in invalid area', async () => {
+    it('GET / => get candidates info in invalid area', async () => {
         const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/candidates/1`)
         expect(res.status).toBe(404)
         expect(res.body).not.toBeNull()
@@ -86,7 +87,7 @@ describe('Test ec /candidates/{id}', () => {
           )
       });
 
-      it('GET / => get candidates info in invalid area (char)', async () => {
+    it('GET / => get candidates info in invalid area (char)', async () => {
         const invalidCandidateIdChar = 'a'
         const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/candidates/${invalidCandidateIdChar}`)
         expect(res.status).toBe(422)
@@ -94,15 +95,63 @@ describe('Test ec /candidates/{id}', () => {
 })
 
 describe('Test ec /party', () => {
-
+    it('GET / => get all party info', async () => {
+        const res = await request(`${ELECTION_COMMITTEE_HOST}`).get('/party')
+        expect(res.status).toBe(200)
+        expect(res.error).toBe(false)
+        expect(res.body).not.toBeNull()
+        for (let i=0; i<res.body.length; i++) {
+            const indexParty = res.body[i]
+            expect(indexParty.id).not.toBeNull()
+            expect(indexParty.name).not.toBeNull()
+            expect(indexParty.pictureUrl).not.toBeNull()
+        }
+      });
 })
 
 describe('Test ec /party/{partyId}', () => {
+    it('GET / => get party info in valid party', async () => {
+        const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/party/${validPartyId}`)
+        expect(res.status).toBe(200)
+        expect(res.error).toBe(false)
+        expect(res.body).not.toBeNull()
+        expect(res.body.id).not.toBeNull()
+        expect(res.body.name).not.toBeNull()
+        expect(res.body.pictureUrl).not.toBeNull()
 
+        expect(res.body.id).toBe(validPartyId)    
+      });
+
+    it('GET / => get party info in invalid party', async () => {
+        const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/party/3`)
+        expect(res.status).toBe(200)
+        expect(res.body).toBeNull()
+      });
+
+    it('GET / => get party info in invalid party (char)', async () => {
+        const invalidPartyIdChar = 'a'
+        const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/party/${invalidPartyIdChar}`)
+        expect(res.status).toBe(422)
+      });
 })
 
 describe('Test ec /party/member/{partyId}', () => {
+    it('GET / => get all member info in party', async () => {
+        const res = await request(`${ELECTION_COMMITTEE_HOST}`).get(`/party/member/${validCandidatesId}`)
+        expect(res.status).toBe(200)
+        expect(res.error).toBe(false)
+        expect(res.body).not.toBeNull()
+        for (let i=0; i<res.body.length; i++) {
+            const indexCandidate = res.body[i]
+            expect(indexCandidate.id).not.toBeNull()
+            expect(indexCandidate.name).not.toBeNull()
+            expect(indexCandidate.pictureUrl).not.toBeNull()
+            expect(indexCandidate.area_id).not.toBeNull()
+            expect(indexCandidate.party_id).not.toBeNull()
 
+            expect(indexCandidate.party_id).toBe(validCandidatesId)
+        }
+      });
 })
 
 describe('Test ec /vote', () => {
